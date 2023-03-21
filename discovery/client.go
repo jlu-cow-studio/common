57d203abab9347269a041391e3ce4d4494dfb32a
waynelee-lwc <1418551940@qq.com>
2023-03-21 10:36:03 +0800

    discover

--- discovery/client.go
+++ discovery/client.go
@@ -1,1 +1,17 @@
 package discovery
+
+import "fmt"
+
+func Discover(serviceName, tag string) ([]string, error) {
+
+	services, _, err := consulClient.Catalog().Service(serviceName, tag, nil)
+	if err != nil {
+		return nil, nil
+	}
+	serviceNames := []string{}
+	for _, service := range services {
+		serviceNames = append(serviceNames, fmt.Sprintf("%v:%v", service.ServiceAddress, service.ServicePort))
+	}
+
+	return serviceNames, nil
+}
