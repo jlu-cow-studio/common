c487c6349bc415bfec870b5fd49058c6344c71ec
waynelee-lwc <1418551940@qq.com>
2023-03-22 17:56:51 +0800

    rpc dial

---
+++ dal/rpc/dial.go
@@ -0,0 +1,33 @@
+package rpc
+
+import (
+	"sync"
+
+	"github.com/jlu-cow-studio/common/discovery"
+	"google.golang.org/grpc"
+)
+
+var connPool = map[string]*grpc.ClientConn{}
+var mutex = &sync.Mutex{}
+
+func GetConn(serviceName string) (*grpc.ClientConn, error) {
+	mutex.Lock()
+	defer mutex.Unlock()
+	if conn, ok := connPool[serviceName]; ok {
+		return conn, nil
+	}
+
+	addrs, err := discovery.Discover(serviceName, "")
+	if err != nil {
+		return nil, err
+	}
+
+	addr := addrs[0]
+
+	if conn, err := grpc.Dial(addr); err != nil {
+		return nil, err
+	} else {
+		connPool[serviceName] = conn
+		return conn, nil
+	}
+}
