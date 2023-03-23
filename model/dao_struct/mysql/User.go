d8df17e0041cfdcea941e88c88641c5257194298
waynelee-lwc <1418551940@qq.com>
2023-03-23 15:28:35 +0800

    user struct gorm

---
+++ model/dao_struct/mysql/User.go
@@ -0,0 +1,11 @@
+package mysql
+
+type User struct {
+	Uid      string `gorm:"column:uid"`
+	Username string `gorm:"column:username"`
+	Password string `gorm:"column:password"`
+	Province string `gorm:"column:province"`
+	City     string `gorm:"column:city"`
+	District string `gorm:"column:district"`
+	Role     int    `gorm:"column:role"`
+}
