d2179b6d9dda7faa03a97884b2a8a1ad21600219
waynelee-lwc <1418551940@qq.com>
2023-03-23 18:58:06 +0800

    user info

--- model/http_struct/FilterInfo.go
+++ model/http_struct/FilterInfo.go
@@ -12,7 +12,7 @@ type UserTokenInfo struct {
 	Token    string `json:"token"`
 	Uid      string `json:"uid"`
 	Username string `json:"username"`
-	Role     int    `json:"role:"`
+	Role     string `json:"role:"`
 	Province string `json:"province"`
 	City     string `json:"city"`
 	District string `json:"district"`
