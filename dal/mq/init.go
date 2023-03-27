dae876a01e671873ac4101ad3ea30527442f30bf
waynelee-lwc <1418551940@qq.com>
2023-03-27 10:57:40 +0800

    init topics

--- dal/mq/init.go
+++ dal/mq/init.go
@@ -1,6 +1,9 @@
 package mq
 
-import "github.com/jlu-cow-studio/common/discovery"
+import (
+	"github.com/jlu-cow-studio/common/discovery"
+	"github.com/segmentio/kafka-go"
+)
 
 const (
 	Topic_ItemChange  = "item_change"
@@ -20,4 +23,39 @@ func Init() {
 
 	addressConsumer = addresses[0]
 	addressProducer = addresses[0]
+
+	topics := []string{
+		Topic_ItemChange,
+		Topic_ClientEvent,
+	}
+
+	for _, topic := range topics {
+		err := RegisterTopic(addressProducer, topic)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func RegisterTopic(address, topic string) error {
+	conn, err := kafka.Dial("tcp", address)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	topicConfigs := []kafka.TopicConfig{
+		{
+			Topic:             topic,
+			NumPartitions:     1,
+			ReplicationFactor: 1,
+		},
+	}
+
+	err = conn.CreateTopics(topicConfigs...)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
