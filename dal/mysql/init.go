1aee4c10e2f531c8faf9c13465a3d64b289f6af6
waynelee-lwc <1418551940@qq.com>
2023-03-28 17:48:21 +0800

    gorm log

--- dal/mysql/init.go
+++ dal/mysql/init.go
@@ -7,6 +7,7 @@ import (
 	"github.com/jlu-cow-studio/common/config"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 var dbPool []*gorm.DB
@@ -19,7 +20,11 @@ func Init() {
 
 	for i := 0; i < config.DBPoolSize; i++ {
 		var err error
-		if dbPool[i], err = gorm.Open(mysql.Open(dsn), &gorm.Config{}); err != nil {
+		if dbPool[i], err = gorm.Open(mysql.Open(dsn), &gorm.Config{
+			// SkipDefaultTransaction: true, // 禁用默认事务
+			Logger:      logger.Default,
+			PrepareStmt: true, // 创建并缓存预编译语句
+		}); err != nil {
 			panic("failed to connect database")
 		}
 	}
