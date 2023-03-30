2aa23ba1e63ce52f3d6b9ee41ac3e1c07441dc2a
waynelee-lwc <1418551940@qq.com>
2023-03-30 11:31:09 +0800

    rpc

--- model/http_struct/item/Item.go
+++ model/http_struct/item/Item.go
@@ -26,6 +26,28 @@ type ItemInfo struct {
 	SpecificAttributes string  `json:"specific_attributes"`
 }
 
+type ItemForFeed struct {
+	ID                 int     `json:"id"`
+	Name               string  `json:"name"`
+	Description        string  `json:"description"`
+	Category           string  `json:"category"`
+	Price              float64 `json:"price"`
+	Stock              int     `json:"stock"`
+	Province           string  `json:"province"`
+	City               string  `json:"city"`
+	District           string  `json:"district"`
+	ImageURL           string  `json:"image_url"`
+	UserID             int     `json:"user_id"`
+	UserType           string  `json:"user_type"`
+	SpecificAttributes string  `json:"specific_attributes"`
+	UID                int     `json:"uid"`
+	Username           string  `json:"username"`
+	UProvince          string  `json:"uprovince"`
+	UCity              string  `json:"ucity"`
+	UDistrict          string  `json:"udistrict"`
+	URole              string  `json:"urole"`
+}
+
 type AddItemReq struct {
 	Base         http_struct.ReqBase `json:"base"`
 	Name         string              `json:"name"`
