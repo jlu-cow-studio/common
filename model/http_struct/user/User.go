9f03cca528aeddc720d3ea16781a28ced0ac3c46
waynelee-lwc <1418551940@qq.com>
2023-04-01 10:27:20 +0800

    user

--- model/http_struct/user/User.go
+++ model/http_struct/user/User.go
@@ -1,6 +1,9 @@
 package user
 
-import "github.com/jlu-cow-studio/common/model/http_struct"
+import (
+	"github.com/jlu-cow-studio/common/model/dao_struct/redis"
+	"github.com/jlu-cow-studio/common/model/http_struct"
+)
 
 const (
 	RoleNormal          = "normal"
@@ -9,6 +12,9 @@ const (
 	RoleVeterinary      = "veterinary"
 	RoleBreeder         = "breeder"
 	RoleServiceProvider = "service_provider"
+
+	FollowAction_Follow   = "follow"
+	FollowAction_UnFollow = "unfollow"
 )
 
 type UserLoginReq struct {
@@ -55,4 +61,50 @@ type UserAuthReq struct {
 
 type UserAuthRes struct {
 	Base http_struct.ResBase `json:"base"`
+}
+
+type UserFollowReq struct {
+	Base        http_struct.ReqBase `json:"base"`
+	FollowingId string              `json:"following_id"`
+	Action      string              `json:"action"`
+}
+
+type UserFollowRes struct {
+	Base http_struct.ResBase `json:"base"`
+}
+
+type UserFollowingReq struct {
+	Base     http_struct.ReqBase `json:"base"`
+	Page     int                 `json:"page"`
+	PageSize int                 `json:"page_size"`
+}
+
+type UserFollowingRes struct {
+	Base       http_struct.ResBase `json:"base"`
+	TotalCount int                 `json:"total_count"`
+	TotalPage  int                 `json:"total_page"`
+	Users      []*redis.UserInfo   `json:"users"`
+}
+
+type UserFollowerReq struct {
+	Base     http_struct.ReqBase `json:"base"`
+	Page     int                 `json:"page"`
+	PageSize int                 `json:"page_size"`
+}
+
+type UserFollowerRes struct {
+	Base       http_struct.ResBase `json:"base"`
+	TotalCount int                 `json:"total_count"`
+	TotalPage  int                 `json:"total_page"`
+	Users      []*redis.UserInfo   `json:"users"`
+}
+
+type UserFollowCountReq struct {
+	Base http_struct.ReqBase `json:"base"`
+}
+
+type UserFollowCountRes struct {
+	Base           http_struct.ResBase `json:"base"`
+	FollowingCount int                 `json:"following_count"`
+	FollowerCount  int                 `json:"follower_count"`
 }
