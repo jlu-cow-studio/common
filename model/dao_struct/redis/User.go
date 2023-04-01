95e71d979cdfdbfb8e869e60ef0a094f4e65eb04
waynelee-lwc <1418551940@qq.com>
2023-04-01 10:30:57 +0800

    user

--- model/dao_struct/redis/User.go
+++ model/dao_struct/redis/User.go
@@ -4,6 +4,7 @@ type UserInfo struct {
 	Uid      string `json:"uid"`
 	Username string `json:"username"`
 	Province string `json:"province"`
+	ImageURL string `json:"image_url"`
 	City     string `json:"city"`
 	District string `json:"district"`
 	Role     string `json:"role"`
