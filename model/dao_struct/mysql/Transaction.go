0bac99b8ebb3b72e88391d8d410b640f8e657de1
waynelee-lwc <1418551940@qq.com>
2023-04-02 16:36:50 +0800

    struct models

--- model/dao_struct/mysql/Transaction.go
+++ model/dao_struct/mysql/Transaction.go
@@ -3,11 +3,11 @@ package mysql
 import "time"
 
 type Transaction struct {
-	ID          uint64    `gorm:"primary_key;column:id"`
-	WalletID    uint64    `gorm:"column:wallet_id"`
+	ID          int64     `gorm:"primary_key;column:id"`
+	WalletID    int64     `gorm:"column:wallet_id"`
 	Type        string    `gorm:"type:ENUM('deposit', 'purchase');column:type"`
 	Amount      float64   `gorm:"type:decimal(10,2);column:amount"`
-	OrderID     uint64    `gorm:"column:order_id"`
+	OrderID     int64     `gorm:"column:order_id"`
 	Description string    `gorm:"type:varchar(255);column:description"`
 	CreatedAt   time.Time `gorm:"column:created_at"`
 }
