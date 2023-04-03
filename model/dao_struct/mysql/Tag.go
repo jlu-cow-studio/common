c993d99e4a2fa64b6aaf9982ac7d6eef244c6e0b
waynelee-lwc <1418551940@qq.com>
2023-04-03 10:23:43 +0800

    tag struct

--- model/dao_struct/mysql/Tag.go
+++ model/dao_struct/mysql/Tag.go
@@ -1,8 +1,8 @@
 package mysql
 
 type Tag struct {
-	ID         int64   `gorm:"column:id;primaryKey;autoIncrement"`
-	Name       string  `gorm:"column:name;not null"`
+	ID         int64   `gorm:"column:tag_id;primaryKey;autoIncrement"`
+	Name       string  `gorm:"column:tag_name;not null"`
 	Weight     float64 `gorm:"column:weight;not null;type:decimal(8,4);default:0.0000"`
 	MarkObject string  `gorm:"column:mark_object;not null"`
 	CategoryID int64   `gorm:"column:category_id;not null"`
@@ -13,15 +13,15 @@ func (Tag) TableName() string {
 }
 
 type TagCategory struct {
-	ID       int    `gorm:"column:id;primaryKey;autoIncrement"`
-	Name     string `gorm:"column:name"`
+	ID       int    `gorm:"column:tag_category_id;primaryKey;autoIncrement"`
+	Name     string `gorm:"column:tag_category_name"`
 	ParentID int    `gorm:"column:parent_id"`
 	Level    int8   `gorm:"column:level"`
 }
 
 type TagWithCategory struct {
-	ID         int64        `gorm:"column:id;primaryKey;autoIncrement"`
-	Name       string       `gorm:"column:name"`
+	ID         int64        `gorm:"column:tag_id;primaryKey;autoIncrement"`
+	Name       string       `gorm:"column:tag_name"`
 	Weight     float64      `gorm:"column:weight"`
 	MarkObject string       `gorm:"column:mark_object"`
 	CategoryID int64        `gorm:"column:category_id"`
