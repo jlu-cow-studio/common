71613cb1c2552097f7ef791147b0ef2da9e0b10d
waynelee-lwc <1418551940@qq.com>
2023-04-05 19:39:52 +0800

    get cli methods

--- dal/rpc/dial.go
+++ dal/rpc/dial.go
@@ -3,12 +3,29 @@ package rpc
 import (
 	"sync"
 
+	"github.com/jlu-cow-studio/common/dal/rpc/feed_service"
+	"github.com/jlu-cow-studio/common/dal/rpc/pack"
+	"github.com/jlu-cow-studio/common/dal/rpc/product_core"
+	"github.com/jlu-cow-studio/common/dal/rpc/tag_core"
+	"github.com/jlu-cow-studio/common/dal/rpc/trade_core"
+	"github.com/jlu-cow-studio/common/dal/rpc/user_core"
 	"github.com/jlu-cow-studio/common/discovery"
 	"google.golang.org/grpc"
 )
 
 var connPool = map[string]*grpc.ClientConn{}
 var mutex = &sync.Mutex{}
+
+const (
+	ServiceName_UserCore        = "cowstudio/user-core"
+	ServiceName_ProductCore     = "cowstudio/product-core"
+	ServiceName_TagCore         = "cowstudio/tag-core"
+	ServiceName_TradeCore       = "cowstudio/trade-core"
+	ServiceName_DataCollector   = "cowstudio/Data-collector"
+	ServiceName_Pack            = "cowstudio/pack"
+	ServiceName_Feed            = "cowstudio/feed"
+	ServiceName_RecommenderCore = "cowstudio/recommender-core"
+)
 
 func GetConn(serviceName string) (*grpc.ClientConn, error) {
 	mutex.Lock()
@@ -31,3 +48,76 @@ func GetConn(serviceName string) (*grpc.ClientConn, error) {
 		return conn, nil
 	}
 }
+
+func GetUserCoreCli() (user_core.UserCoreServiceClient, error) {
+	conn, err := GetConn(ServiceName_UserCore)
+	if err != nil {
+		return nil, err
+	}
+
+	return user_core.NewUserCoreServiceClient(conn), nil
+
+}
+
+func GetProductCoreCli() (product_core.ProductCoreServiceClient, error) {
+	conn, err := GetConn(ServiceName_ProductCore)
+	if err != nil {
+		return nil, err
+	}
+
+	return product_core.NewProductCoreServiceClient(conn), nil
+}
+
+func GetTagCoreCli() (tag_core.TagCoreServiceClient, error) {
+	conn, err := GetConn(ServiceName_TagCore)
+	if err != nil {
+		return nil, err
+	}
+
+	return tag_core.NewTagCoreServiceClient(conn), nil
+}
+
+func GetTradeCoreCli() (trade_core.TreadeCoreServiceClient, error) {
+	conn, err := GetConn(ServiceName_TradeCore)
+	if err != nil {
+		return nil, err
+	}
+
+	return trade_core.NewTreadeCoreServiceClient(conn), nil
+}
+
+// func GetDataCollectorCli() (data_collector.DataCollectorServiceClient, error) {
+// 	conn, err := GetConn(ServiceName_DataCollector)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return data_collector.NewDataCollectorServiceClient(conn), nil
+// }
+
+func GetPackCli() (pack.PackServiceClient, error) {
+	conn, err := GetConn(ServiceName_Pack)
+	if err != nil {
+		return nil, err
+	}
+
+	return pack.NewPackServiceClient(conn), nil
+}
+
+func GetFeedCli() (feed_service.FeedServiceClient, error) {
+	conn, err := GetConn(ServiceName_Feed)
+	if err != nil {
+		return nil, err
+	}
+
+	return feed_service.NewFeedServiceClient(conn), nil
+}
+
+// func GetRecommenderCoreCli() (recommender_core.RecommenderCoreServiceClient, error) {
+// 	conn, err := GetConn(ServiceName_RecommenderCore)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+
+// 	return recommender_core.NewRecommenderCoreServiceClient(conn), nil
+// }
